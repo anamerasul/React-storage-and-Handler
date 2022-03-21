5e5fc9888e63bd5c1529632f5075968708279287
anamerasul <anamerasul@gmail.com>
2022-03-22 00:59:25 +0600

    Module Introduction ES6 Modules simple import, export

---
+++ react-storage-and-handler/src/App.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+import './App.css';
+import Cosmatic from './Components/Cosmatic/Cosmatic';
+
+function App() {
+  return (
+    <div className="App">
+    <Cosmatic></Cosmatic>
+    </div>
+  );
+}
+
+export default App;
---
+++ react-storage-and-handler/src/Components/Cosmatic/Cosmatic.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Cosmatic.css'
+const Cosmatic = () => {
+    return (
+        <div>
+            <h1>Welcome to my cosmatic</h1>
+        </div>
+    );
+};
+
+export default Cosmatic;
