a45f15ebd04f8f1a0ebcc9ab077932bd4c7310da
anamerasul <anamerasul@gmail.com>
2022-03-22 03:32:02 +0600

    47_5-6 Store multiple data in an Object with local storage

--- react-storage-and-handler/src/Components/Utlities/fakedb.js
+++ react-storage-and-handler/src/Components/Utlities/fakedb.js
@@ -1,19 +1,45 @@
 //use lacal stroage to manage cart data
 
 const addToDb=id=>{
-    const quantity=localStorage.getItem(id)
+
+let shoppingCart={}
+
+
+// get the shopping cart
+const storedCart=localStorage.getItem('shopping-Cart')
+
+if (storedCart){
+ shoppingCart=JSON.parse(storedCart)
+
+}
+
+else{
+    shoppingCart={}
+}
+
+
+    // const quantity=localStorage.getItem(id)
+
+    //add quantity
+    const quantity=shoppingCart[id]
     if(quantity){
-console.log('alreday exsits')
-const newQuantity=+quantity+1
+// console.log('alreday exsits')
+// const newQuantity=+quantity+1
+const newQuantity=quantity+1
+shoppingCart[id]=newQuantity
+
 // const newQuantity=(parseInt(quantity))+1;
-localStorage.setItem(id,newQuantity)
+// localStorage.setItem(id,newQuantity)
+
     }
 
     else{
-        console.log('new item')
-        localStorage.setItem(id,1)
+
+        shoppingCart[id]=1;
+        // console.log('new item')
+        // localStorage.setItem(id,1)
     }
-    
+    localStorage.setItem('shopping-Cart',JSON.stringify(shoppingCart))
 }
 
 export {addToDb}
